fb15c41a5759d81783de4493a22f77fd536322c6
saidevops7722 <saiaws7722@gmail.com>
2024-04-10 15:14:41 +0530

    jenkins provider info

--- vars/common.groovy
+++ vars/common.groovy
@@ -15,5 +15,8 @@ def testcases() {
 }
 
 def codequality() {
-    sh 'sonar-scanner -Dsonar.host.url=http://172.31.39.21:9000 -Dsonar.login=admin -Dsonar.password=admin123 -Dsonar.projectKey=${component} ${sonar_extra_opts}'
+    withAWSParameterStore(credentialsId: 'PARAM1', naming: 'absolute', path: '/sonarqube', recursive: true, regionName: 'us-east-1')
+            {
+                sh 'sonar-scanner -Dsonar.host.url=http://172.31.39.21:9000 -Dsonar.login=${SONARQUBE_USER} -Dsonar.password=${SONARQUBE_PASS} -Dsonar.projectKey=${component} ${sonar_extra_opts}'
+            }
 }
