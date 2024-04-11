2760a27a93861c993ca12581bd8bd37039a03f13
saidevops7722 <saiaws7722@gmail.com>
2024-04-11 17:08:40 +0530

    jenkins provider info

---
+++ vars/ci_scripted.groovy
@@ -0,0 +1,43 @@
+def call() {
+    if (!env.sonar_extra_opts) {
+        env.sonar_extra_opts = ""
+    }
+
+    if(env.TAG_NAME ==~ ".*") {
+        env.GTAG = "true"
+    } else {
+        env.GTAG = "false"
+    }
+    node('workstation') {
+
+        try {
+            stage('Check out Code') {
+                cleanWs()
+                git branch: 'main', url: 'https://github.com/SaiDevOps27/cart.git'
+            }
+
+            sh 'env'
+
+            if(env.BRANCH_NAME != main) {
+                stage('Build/Compile') {
+                    common.compile()
+                }
+            }
+
+            println GTAG
+            println BRANCH_NAME
+
+            if(env.GTAG != "true" && env.BRANCH_NAME != "main") {
+                stage('Test Cases') {
+                    common.testcases()
+                }
+            }
+
+            stage('Code Quality') {
+                common.codequality()
+            }
+        } catch(e) {
+            mail body: "<h1>${component} - PipeLine Failed \n ${BUILD_URL}<h1>", from: 'saimaheshgundu@gmail.com', subject: "${component} - pipeline failed", to: 'saimaheshgundu@gmail.com', mimeType: 'text/html'
+        }
+    }
+}
