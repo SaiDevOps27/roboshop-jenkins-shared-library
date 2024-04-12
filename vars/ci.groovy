ea92a45bc74d592061f92d368c0df4a4a3b7877f
saidevops7722 <saiaws7722@gmail.com>
2024-04-12 17:17:00 +0530

    jenkins provider info

--- vars/ci.groovy
+++ vars/ci.groovy
@@ -1,42 +1,56 @@
 def call() {
     if (!env.sonar_extra_opts) {
-        env.sonar_extra_opts=""
+        env.sonar_extra_opts = ""
     }
-    pipeline {
-        agent any
+    if (env.TAG_NAME ==~ ".*") {
+        env.GTAG = "true"
+    } else {
+        env.GTAG = "false"
+    }
+    node('workstation') {
 
-        stages {
+        try {
 
-            stage('Compile/Build') {
-                steps {
-                    script {
-                        common.compile()
-                    }
-                }
-            }
-            stage('Test Cases') {
-                steps {
-                    script {
-                        common.testcases()
-                    }
-                }
+            stage('Check out Code') {
+                cleanWs()
+                git branch: 'main', url: 'https://github.com/SaiDevOps27/cart.git'
             }
 
-            stage('Code Quality') {
-                steps {
-                    script {
-                        common.codequality()
-                    }
+            sh 'env'
+
+            if (env.BRANCH_NAME != "main") {
+                stage('Build/Compile') {
+                    common.compile()
+                }
+            }
+
+            println GTAG
+            println BRANCH_NAME
+
+            if (env.GTAG != "true" && env.BRANCH_NAME != "main") {
+                stage('Test Cases') {
+                    common.testcases()
+                }
+            }
+
+            if (BRANCH_NAME ==~ "PR-.*" ) {
+                stage('Code Quality') {
+                    common.codequality()
+                }
+            }
+
+            if (env.GTAG == "true") {
+                stage('Package') {
+                    common.prepareArtifacts()
+                }
+                stage('Artifact Upload') {
+                    common.testcases()
                 }
             }
         }
-
-        post {
-            failure {
-                mail body: "<h1>${component} - PipeLine Failed \n ${BUILD_URL}<h1>", from: 'saimaheshgundu@gmail.com', subject: "${component} - pipeline failed", to: 'saimaheshgundu@gmail.com', mimeType: 'text/html'
-            }
+        catch(e) {
+            mail body: "<h1>${component} - PipeLine Failed \n ${BUILD_URL}<h1>", from: 'saimaheshgundu@gmail.com', subject: "${component} - pipeline failed", to: 'saimaheshgundu@gmail.com', mimeType: 'text/html'
         }
-
     }
 }
 
---
+++ vars/ci_old.groovy
@@ -0,0 +1,43 @@
+def call() {
+    if (!env.sonar_extra_opts) {
+        env.sonar_extra_opts=""
+    }
+    pipeline {
+        agent any
+
+        stages {
+
+            stage('Compile/Build') {
+                steps {
+                    script {
+                        common.compile()
+                    }
+                }
+            }
+            stage('Test Cases') {
+                steps {
+                    script {
+                        common.testcases()
+                    }
+                }
+            }
+
+            stage('Code Quality') {
+                steps {
+                    script {
+                        common.codequality()
+                    }
+                }
+            }
+        }
+
+        post {
+            failure {
+                mail body: "<h1>${component} - PipeLine Failed \n ${BUILD_URL}<h1>", from: 'saimaheshgundu@gmail.com', subject: "${component} - pipeline failed", to: 'saimaheshgundu@gmail.com', mimeType: 'text/html'
+            }
+        }
+
+    }
+}
+
+
