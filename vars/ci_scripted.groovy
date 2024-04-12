62642605753771b900b676d1454edcf71b27aace
saidevops7722 <saiaws7722@gmail.com>
2024-04-12 10:27:19 +0530

    jenkins provider info

--- vars/ci_scripted.groovy
+++ vars/ci_scripted.groovy
@@ -6,11 +6,8 @@ def call() {
 
         try {
             stage('Check out Code') {
-                sh 'ls -l'
                 cleanWs()
-                sh 'ls -l'
                 git branch: 'main', url: 'https://github.com/SaiDevOps27/cart.git'
-                sh 'ls -l'
             }
 
             sh 'env'
