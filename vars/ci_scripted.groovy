bc77bfe823e43b5bc49430847161b3cbc99f3e83
saidevops7722 <saiaws7722@gmail.com>
2024-04-12 09:23:50 +0530

    jenkins provider info

--- vars/ci_scripted.groovy
+++ vars/ci_scripted.groovy
@@ -2,51 +2,30 @@ def call() {
     if (!env.sonar_extra_opts) {
         env.sonar_extra_opts = ""
     }
-
-    if(env.TAG_NAME ==~ ".*") {
-        env.GTAG = "true"
-    } else {
-        env.GTAG = "false"
-    }
     node('workstation') {
 
         try {
             stage('Check out Code') {
+                sh 'ls -l'
                 cleanWs()
+                sh 'ls -l'
                 git branch: 'main', url: 'https://github.com/SaiDevOps27/cart.git'
+                sh 'ls -l'
             }
 
-            sh 'env'
-
-            if(env.BRANCH_NAME != main) {
-                stage('Build/Compile') {
+            stage('Build/Compile') {
+                    sh 'env'
                     common.compile()
                 }
-            }
 
-            println GTAG
-            println BRANCH_NAME
-
-            if(env.GTAG != "true" && env.BRANCH_NAME != "main") {
-                stage('Test Cases') {
+            stage('Test Cases') {
                     common.testcases()
                 }
-            }
 
-            if(BRANCH_NAME ==~ "PR-.*") {
-                stage('Code Quality') {
+            stage('Code Quality') {
                     common.codequality()
                 }
-            }
 
-            if(env.GTAG == "true") {
-                stage('Packages') {
-                    common.testcases()
-                }
-                stage('Artifacts Upload') {
-                    common.testcases()
-                }
-            }
         } catch(e) {
             mail body: "<h1>${component} - PipeLine Failed \n ${BUILD_URL}<h1>", from: 'saimaheshgundu@gmail.com', subject: "${component} - pipeline failed", to: 'saimaheshgundu@gmail.com', mimeType: 'text/html'
         }
