36597438d1ea8c3c0cb25c2be4daf013d5c5168c
saidevops7722 <saiaws7722@gmail.com>
2024-04-13 11:35:27 +0530

    jenkins provider info

--- vars/terraform.groovy
+++ vars/terraform.groovy
@@ -4,6 +4,7 @@ def call() {
 
         parameters {
             string(name: 'ENV', defaultValue: '', description: 'Which Environment?')
+            string(name: 'ACTION', defaultValue: '', description: 'Which Action?')
         }
 
         options {
@@ -31,3 +32,4 @@ def call() {
         }
     }
 }
+
